943ede462a3e26980a0d5b5572ee392b5891b004
firefistdev <akaki7600@gmail.com>
2023-09-24 21:43:04 +0400

    finish administrator

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -4,10 +4,8 @@ import { UserController } from './user.controller';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { CarModule } from './car/car.module';
 import { JwtStrategy } from '../JWT/jwt.strategy';
-import { ParkCarModule } from './park-car/park-car.module';
-
 @Module({
-  imports: [PrismaModule, CarModule, ParkCarModule],
+  imports: [PrismaModule, CarModule],
   controllers: [UserController],
   providers: [UserService,JwtStrategy],
 })
